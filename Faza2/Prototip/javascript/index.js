de3a6725b1d71b23f5e30269b643526e863faae4
Marko Mirkovic <markomiroslavmirkovic@gmail.com>
2022-04-11 19:19:37 +0200

    Dodata responzivnost pocetnoj stranici.

--- Faza2/Prototip/javascript/index.js
+++ Faza2/Prototip/javascript/index.js
@@ -1,8 +1,33 @@
 // Marko Mirković 197/19
+
 document.addEventListener("DOMContentLoaded", function() {
+    function windowResize() {
+        var leviPanel = document.getElementById("leviPanel");
+        var leviPanelWidth = getComputedStyle(leviPanel).width;
+        leviPanelWidth = parseInt(leviPanelWidth.substring(0, leviPanelWidth.length - 1));
+        if(leviPanelWidth > window.innerWidth * 0.6) {
+            leviPanel.style.marginLeft = "-200vw";
+        } else {
+            leviPanel.style.marginLeft = "0";
+        }
+    }
+    
+    windowResize();
+    window.addEventListener('resize', windowResize);
+
     document.getElementById("zaboravljenaSifra").addEventListener('click', function() {
         document.getElementById("zaboravljenaSifraPozadina").style.display = "block";
         document.getElementById("zaboravljenaSifraPanelUsername").value = "";
+    });
+
+    document.getElementById("zaboravljenaSifraPozadina").addEventListener("click", function(ev) {
+        if(ev.target !== ev.currentTarget) return;
+        document.getElementById("zaboravljenaSifraPozadina").style.display = "none";
+    });
+
+    document.getElementById("zaboravljenaSifraPotvrdi").addEventListener("click", function(ev) {
+        if(ev.target !== ev.currentTarget) return;
+        document.getElementById("zaboravljenaSifraPozadina").style.display = "none";
     });
 });
 
@@ -18,41 +43,14 @@ function predjiNaStranicu(register) {
 
 
 function animacijaRegistracijeNastaje() {
-    document.getElementById("desniPanelWrapper").style.animationName = "animacijaRegistracijaWrapperGore";
-    document.getElementById("desniPanelWrapper").style.animationPlayState = "running";
-    
-    document.getElementById("desniPanelRegistracija").style.animationName = "animacijaRegistracijaNastaje";
-    document.getElementById("desniPanelRegistracija").style.animationPlayState = "running";
-
-    document.getElementById("desniPanel").style.animationName = "animacijaLoginNestaje";
-    document.getElementById("desniPanel").style.animationPlayState = "running";
+    var desniPanel = document.getElementById("desniPanel");
+    desniPanel.style.animationName = "animacijaLoginNestaje";
+    desniPanel.style.animationPlayState = "running";
 }
 
 
 function animacijaRegistracijeNestaje() {
-    document.getElementById("desniPanelWrapper").style.animationName = "animacijaRegistracijaWrapperDole";
-    document.getElementById("desniPanelWrapper").style.animationPlayState = "running";
-
-    document.getElementById("desniPanelRegistracija").style.animationName = "animacijaRegistracijaNestaje";
-    document.getElementById("desniPanelRegistracija").style.animationPlayState = "running";
-
-    document.getElementById("desniPanel").style.animationName = "animacijaLoginNastaje";
-    document.getElementById("desniPanel").style.animationPlayState = "running";
-
+    var desniPanel = document.getElementById("desniPanel");
+    desniPanel.style.animationName = "animacijaLoginNastaje";
+    desniPanel.style.animationPlayState = "running";
 }
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("zaboravljenaSifraPozadina").addEventListener("click", function(ev) {
-        if(ev.target !== ev.currentTarget) return;
-        document.getElementById("zaboravljenaSifraPozadina").style.display = "none";
-    });
-});
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("zaboravljenaSifraPotvrdi").addEventListener("click", function(ev) {
-        if(ev.target !== ev.currentTarget) return;
-        document.getElementById("zaboravljenaSifraPozadina").style.display = "none";
-    });
-});

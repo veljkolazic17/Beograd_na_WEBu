d45980b14816001e96c6008e533f4fee00b53833
LV <veljkolazic117@gmail.com>
2022-04-27 21:20:08 +0200

    Dodat dependency za thymeleaf!

---
+++ server/BeogradNaWebu/src/main/java/rs/psi/beogradnawebu/controller/FilterController.java
@@ -0,0 +1,16 @@
+package rs.psi.beogradnawebu.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import rs.psi.beogradnawebu.model.Smestaj;
+import java.util.List;
+
+@Controller
+public class FilterController {
+
+
+    
+
+
+    
+}

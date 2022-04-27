46b3f7d8b0f809aa6f884640b8694fd4f2061053
LV <veljkolazic117@gmail.com>
2022-04-27 21:30:04 +0200

    Dodat objectMapper

--- server/BeogradNaWebu/src/main/java/rs/psi/beogradnawebu/controller/FilterController.java
+++ server/BeogradNaWebu/src/main/java/rs/psi/beogradnawebu/controller/FilterController.java
@@ -1,12 +1,22 @@
 package rs.psi.beogradnawebu.controller;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import rs.psi.beogradnawebu.model.Smestaj;
 import java.util.List;
 
+
+
+
 @Controller
 public class FilterController {
+
+    private ObjectMapper objectMapper;
+    FilterController(ObjectMapper objectMapper){
+        this.objectMapper = objectMapper;
+    }
 
 
     

bb2dfce7538370e2ff0720c8a67de528027d8ed4
veljk <veljkolazic117@gmail.com>
2022-05-18 08:06:20 -0700

    Dodat Recalgdata, RecAlgDAO i promenjena je baza kako bi bilo moguce prilagoditi algoritam

---
+++ server/BeogradNaWebu/src/main/java/rs/psi/beogradnawebu/model/Recalgdata.java
@@ -0,0 +1,167 @@
+package rs.psi.beogradnawebu.model;
+
+
+public class Recalgdata {
+
+  private long idkorisnik;
+  private double rangeMinBrojSoba;
+  private double rangeMaxBrojSoba;
+  private long rangeMinSpratnost;
+  private long rangeMaxSpratnost;
+  private long rangeMinImaLift;
+  private long rangeMaxImaLift;
+  private long rangeMinKvadratura;
+  private long rangeMaxKvadratura;
+  private double rangeMinCena;
+  private double rangeMaxCena;
+  private double weightBrojSoba;
+  private double weigthSpratonst;
+  private double weightImaLift;
+  private double weightKvadratura;
+  private double weightCena;
+
+
+  public long getIdkorisnik() {
+    return idkorisnik;
+  }
+
+  public void setIdkorisnik(long idkorisnik) {
+    this.idkorisnik = idkorisnik;
+  }
+
+
+  public double getRangeMinBrojSoba() {
+    return rangeMinBrojSoba;
+  }
+
+  public void setRangeMinBrojSoba(double rangeMinBrojSoba) {
+    this.rangeMinBrojSoba = rangeMinBrojSoba;
+  }
+
+
+  public double getRangeMaxBrojSoba() {
+    return rangeMaxBrojSoba;
+  }
+
+  public void setRangeMaxBrojSoba(double rangeMaxBrojSoba) {
+    this.rangeMaxBrojSoba = rangeMaxBrojSoba;
+  }
+
+
+  public long getRangeMinSpratnost() {
+    return rangeMinSpratnost;
+  }
+
+  public void setRangeMinSpratnost(long rangeMinSpratnost) {
+    this.rangeMinSpratnost = rangeMinSpratnost;
+  }
+
+
+  public long getRangeMaxSpratnost() {
+    return rangeMaxSpratnost;
+  }
+
+  public void setRangeMaxSpratnost(long rangeMaxSpratnost) {
+    this.rangeMaxSpratnost = rangeMaxSpratnost;
+  }
+
+
+  public long getRangeMinImaLift() {
+    return rangeMinImaLift;
+  }
+
+  public void setRangeMinImaLift(long rangeMinImaLift) {
+    this.rangeMinImaLift = rangeMinImaLift;
+  }
+
+
+  public long getRangeMaxImaLift() {
+    return rangeMaxImaLift;
+  }
+
+  public void setRangeMaxImaLift(long rangeMaxImaLift) {
+    this.rangeMaxImaLift = rangeMaxImaLift;
+  }
+
+
+  public long getRangeMinKvadratura() {
+    return rangeMinKvadratura;
+  }
+
+  public void setRangeMinKvadratura(long rangeMinKvadratura) {
+    this.rangeMinKvadratura = rangeMinKvadratura;
+  }
+
+
+  public long getRangeMaxKvadratura() {
+    return rangeMaxKvadratura;
+  }
+
+  public void setRangeMaxKvadratura(long rangeMaxKvadratura) {
+    this.rangeMaxKvadratura = rangeMaxKvadratura;
+  }
+
+
+  public double getRangeMinCena() {
+    return rangeMinCena;
+  }
+
+  public void setRangeMinCena(double rangeMinCena) {
+    this.rangeMinCena = rangeMinCena;
+  }
+
+
+  public double getRangeMaxCena() {
+    return rangeMaxCena;
+  }
+
+  public void setRangeMaxCena(double rangeMaxCena) {
+    this.rangeMaxCena = rangeMaxCena;
+  }
+
+
+  public double getWeightBrojSoba() {
+    return weightBrojSoba;
+  }
+
+  public void setWeightBrojSoba(double weightBrojSoba) {
+    this.weightBrojSoba = weightBrojSoba;
+  }
+
+
+  public double getWeigthSpratonst() {
+    return weigthSpratonst;
+  }
+
+  public void setWeigthSpratonst(double weigthSpratonst) {
+    this.weigthSpratonst = weigthSpratonst;
+  }
+
+
+  public double getWeightImaLift() {
+    return weightImaLift;
+  }
+
+  public void setWeightImaLift(double weightImaLift) {
+    this.weightImaLift = weightImaLift;
+  }
+
+
+  public double getWeightKvadratura() {
+    return weightKvadratura;
+  }
+
+  public void setWeightKvadratura(double weightKvadratura) {
+    this.weightKvadratura = weightKvadratura;
+  }
+
+
+  public double getWeightCena() {
+    return weightCena;
+  }
+
+  public void setWeightCena(double weightCena) {
+    this.weightCena = weightCena;
+  }
+
+}

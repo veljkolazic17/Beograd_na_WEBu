0fdd05074074be691ac397ffa42da44036bbde24
veljk <veljkolazic117@gmail.com>
2022-05-31 23:19:49 +0200

    Dodati parametri za skrejping

--- server/BeogradNaWebu/src/main/java/rs/psi/beogradnawebu/BeogradNaWebuApplication.java
+++ server/BeogradNaWebu/src/main/java/rs/psi/beogradnawebu/BeogradNaWebuApplication.java
@@ -2,8 +2,10 @@ package rs.psi.beogradnawebu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BeogradNaWebuApplication {
 
     public static void main(String[] args) {
